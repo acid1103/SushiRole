1ab84731fa26a3abe5a96f1be96a96c7714c08ea
acid1103 <sir.fon1103@gmail.com>
2018-09-16 08:07:39 -0500

    main method refactor

--- src/main/java/org/abitoff/discord/sushirole/CLICommand.java
+++ src/main/java/org/abitoff/discord/sushirole/CLICommand.java
@@ -1,16 +1,36 @@
 package org.abitoff.discord.sushirole;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 
+import javax.security.auth.login.LoginException;
+
+import org.abitoff.discord.sushirole.config.SushiRoleConfig;
+import org.abitoff.discord.sushirole.config.SushiRoleConfig.BotConfig;
+import org.abitoff.discord.sushirole.events.GlobalEventListener;
+import org.abitoff.discord.sushirole.exceptions.ExceptionHandler;
+import org.abitoff.discord.sushirole.exceptions.FatalException;
 import org.abitoff.discord.sushirole.utils.JCommanderUtils;
 import org.abitoff.discord.sushirole.utils.JCommanderUtils.JCommanderPackage;
 
+import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.Parameters;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
 
 public abstract class CLICommand
 {
@@ -22,6 +42,7 @@ public abstract class CLICommand
 	{
 		private static final boolean defaultDev = false;
 		private static final List<Integer> defaultShard = new ArrayList<Integer>(Arrays.asList(0, 1));
+		private static final int defaultVerbosity = 3;
 
 		@Parameter(names = { "-dev", "--developer_mode" },description = "Boolean value used to indicate which bot to "
 				+ "launch. True launches the dev mode bot, and false launches the public bot. Usage: '-dev flag' "
@@ -30,24 +51,59 @@ public abstract class CLICommand
 
 		@Parameter(names = { "-shard" },description = "Two integers, used to determine the shard ID for this instance "
 				+ "of the bot. Usage: '-shard shardId totalShards' Example: '-shard 5 10' For further information, see "
-				+ "https://discordapp.com/developers/docs/topics/gateway#sharding",arity = 2)
+				+ "https://discordapp.com/developers/docs/topics/gateway#sharding",arity = 2,validateWith = ShardValidator.class)
 		private List<Integer> shard = defaultShard;
 
-		public final boolean devMode()
+		@Parameter(names = { "-v", "-verbose" },description = "An integer describing the verbosity of the messages to "
+				+ "log. Values range from 0-8, with 0 meaning that no messages should be logged, and 8 meaning that all "
+				+ "should be logged. More specifically, 0 turns logging off, 1 corresponds to severe logs, 2 corresponds "
+				+ "to warning logs and up, 3 corresponds to info logs and up, 4 corresponds to config logs and up, 5-7 "
+				+ "correspond to logs of respectively increasing granularity and up, and 8 corresponds to all logs.")
+		private int verbosity = defaultVerbosity;
+
+		public void run()
 		{
-			return dev;
+			Level[] verbosityLevels = new Level[] { Level.OFF, Level.SEVERE, Level.WARNING, Level.INFO, Level.CONFIG,
+					Level.FINE, Level.FINER, Level.FINEST, Level.ALL };
+			File keys = new File("./sushiroleconfig/config");
+
+			GsonBuilder builder = new GsonBuilder();
+			Gson gson = builder.create();
+
+			SushiRoleConfig config;
+			try (BufferedReader br = new BufferedReader(new FileReader(keys)))
+			{
+				config = gson.fromJson(br, SushiRoleConfig.class);
+			} catch (IOException e)
+			{
+				throw new FatalException("Exception while reading config file!", e);
+			}
+
+			BotConfig botAccount = this.dev ? config.discord_bot_dev : config.discord_bot;
+			ExceptionHandler.initiate(config.pastebin);
+
+			try
+			{
+				new JDABuilder(AccountType.BOT).setToken(botAccount.token)
+						.addEventListener(GlobalEventListener.listener).useSharding(shard.get(0), shard.get(1)).build()
+						.awaitReady();
+			} catch (LoginException | InterruptedException e)
+			{
+				throw new FatalException("Exception while starting bot!", e);
+			}
 		}
 
-		public final int shardId()
+		private static final class ShardValidator implements IParameterValidator
 		{
-			return shard.get(0);
-		}
-
-		public final int shardTotal()
-		{
-			return shard.get(1);
+			@Override
+			public void validate(String name, String value) throws ParameterException
+			{
+				System.out.println(value);
+			}
 		}
 	}
+
+	public abstract void run();
 
 	public static final CLICommand parseCommand(String...args) throws Exception
 	{
--- src/main/java/org/abitoff/discord/sushirole/SushiRole.java
+++ src/main/java/org/abitoff/discord/sushirole/SushiRole.java
@@ -1,25 +1,7 @@
 package org.abitoff.discord.sushirole;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import javax.security.auth.login.LoginException;
-
-import org.abitoff.discord.sushirole.config.SushiRoleConfig;
-import org.abitoff.discord.sushirole.config.SushiRoleConfig.BotConfig;
-import org.abitoff.discord.sushirole.events.GlobalEventListener;
-import org.abitoff.discord.sushirole.exceptions.ExceptionHandler;
-
-import com.beust.jcommander.JCommander;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
 
 public class SushiRole
 {
@@ -28,45 +10,8 @@ public class SushiRole
 	public static void main(String[] args) throws Exception
 	{
 		LOG.setLevel(Level.ALL);
-		args = "run -dev tru".split(" ");
-		CLICommand.parseCommand(args);
-		System.exit(0);
-		CLICommand.RunCommand runParams = new CLICommand.RunCommand();
-		JCommander jc = JCommander.newBuilder().addCommand("run", runParams).build();
-		try
-		{
-			jc.parse(args);
-			if (jc.getParsedCommand() == null)
-				throw new Exception();
-		} catch (Exception e)
-		{
-			StringBuilder sb = new StringBuilder();
-			jc.usage("run", sb);
-			System.err.println(sb.toString());
-			throw e;
-		}
-
-		File keys = new File("./sushiroleconfig/config");
-
-		GsonBuilder builder = new GsonBuilder();
-		Gson gson = builder.create();
-
-		SushiRoleConfig config;
-		try (BufferedReader br = new BufferedReader(new FileReader(keys)))
-		{
-			config = gson.fromJson(br, SushiRoleConfig.class);
-		}
-
-		startBot(config, runParams);
-	}
-
-	public static JDA startBot(SushiRoleConfig config, CLICommand.RunCommand runParams)
-			throws LoginException,InterruptedException
-	{
-		BotConfig botAccount = runParams.devMode() ? config.discord_bot_dev : config.discord_bot;
-		ExceptionHandler.initiate(config.pastebin);
-
-		return new JDABuilder(AccountType.BOT).setToken(botAccount.token).addEventListener(GlobalEventListener.listener)
-				.useSharding(runParams.shardId(), runParams.shardTotal()).build().awaitReady();
+		args = "run -dev true".split(" ");
+		CLICommand command = CLICommand.parseCommand(args);
+		command.run();
 	}
 }
