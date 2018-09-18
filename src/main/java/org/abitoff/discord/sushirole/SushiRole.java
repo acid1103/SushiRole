bceafd55757d5e9345324909ba9d473bf01e1dba
acid1103 <sir.fon1103@gmail.com>
2018-09-17 21:17:20 -0500

    begin porting to picocli

--- src/main/java/org/abitoff/discord/sushirole/SushiRole.java
+++ src/main/java/org/abitoff/discord/sushirole/SushiRole.java
@@ -1,7 +1,13 @@
 package org.abitoff.discord.sushirole;
 
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.abitoff.discord.sushirole.cli.CLICommand;
+import org.abitoff.discord.sushirole.cli.CLICommand2;
+
+import picocli.CommandLine;
 
 public class SushiRole
 {
@@ -9,9 +15,13 @@ public class SushiRole
 
 	public static void main(String[] args) throws Exception
 	{
+		CLICommand2.RunCommand cmd = new CLICommand2.RunCommand();
+		new CommandLine(cmd).parse("--dev --shard 5 10".split(" "));
+		System.out.println(Arrays.toString(cmd.shard));
+		System.exit(0);
 		LOG.setLevel(Level.ALL);
 		args = "run -dev true -v 8".split(" ");
 		CLICommand command = CLICommand.parseCommand(args);
-		command.run();
+		command.runCommand();
 	}
 }
--- src/main/java/org/abitoff/discord/sushirole/cli/CLICommand.java
+++ src/main/java/org/abitoff/discord/sushirole/cli/CLICommand.java
@@ -1,4 +1,4 @@
-package org.abitoff.discord.sushirole;
+package org.abitoff.discord.sushirole.cli;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -12,6 +12,7 @@ import java.util.logging.Level;
 
 import javax.security.auth.login.LoginException;
 
+import org.abitoff.discord.sushirole.SushiRole;
 import org.abitoff.discord.sushirole.config.SushiRoleConfig;
 import org.abitoff.discord.sushirole.config.SushiRoleConfig.BotConfig;
 import org.abitoff.discord.sushirole.events.GlobalEventListener;
@@ -23,6 +24,7 @@ import org.abitoff.discord.sushirole.utils.LoggingUtils;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.Parameters;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -32,7 +34,8 @@ import net.dv8tion.jda.core.JDABuilder;
 
 public abstract class CLICommand
 {
-	private static final JCommanderPackage<CLICommand> parsePackage = JCommanderUtils.generateJCommanderPackage(CLICommand.class);
+	private static final JCommanderPackage<CLICommand> parsePackage = JCommanderUtils
+			.generateJCommanderPackage(CLICommand.class);
 
 	@Parameters(commandDescription = "Runs the bot",commandNames = { "run" })
 	public static class RunCommand extends CLICommand
@@ -58,17 +61,10 @@ public abstract class CLICommand
 				+ "correspond to logs of respectively increasing granularity and up, and 8 corresponds to all logs.")
 		private int verbosity = defaultVerbosity;
 
-		public void run()
+		void run()
 		{
-			if (0 > verbosity || verbosity > 8)
-				throw new FatalException(String.format("Verbose value must be in the range [0,8]! (Got %d)", verbosity));
-			if (0 > shard.get(0) || shard.get(0) >= shard.get(1))
-				throw new FatalException(String.format("shardId must be greater than 0, and totalShards must be greater than "
-						+ "shardId! (Got: shardId:%d, totalShards:%d) See https://discordapp.com/developers/docs/topics/gateway#sharding",
-						shard.get(0), shard.get(1)));
-
-			Level[] verbosityLevels = new Level[] { Level.OFF, Level.SEVERE, Level.WARNING, Level.INFO, Level.CONFIG, Level.FINE,
-					Level.FINER, Level.FINEST, Level.ALL };
+			Level[] verbosityLevels = new Level[] { Level.OFF, Level.SEVERE, Level.WARNING, Level.INFO, Level.CONFIG,
+					Level.FINE, Level.FINER, Level.FINEST, Level.ALL };
 			Level verbosityLevel = verbosityLevels[verbosity];
 
 			LoggingUtils.infof("Setting log verbosity to %s.", verbosityLevel.getName());
@@ -93,18 +89,40 @@ public abstract class CLICommand
 
 			try
 			{
-				new JDABuilder(AccountType.BOT).setToken(botAccount.token).addEventListener(GlobalEventListener.listener)
-						.useSharding(shard.get(0), shard.get(1)).build().awaitReady();
+				new JDABuilder(AccountType.BOT).setToken(botAccount.token)
+						.addEventListener(GlobalEventListener.listener).useSharding(shard.get(0), shard.get(1)).build()
+						.awaitReady();
 			} catch (LoginException | InterruptedException e)
 			{
 				throw new FatalException("Exception while starting bot!", e);
 			}
 		}
+
+		void validateParameters()
+		{
+			if (0 > verbosity || verbosity > 8)
+				throw new FatalException(
+						String.format("Verbose value must be in the range [0,8]! (Got %d)", verbosity));
+			if (0 > shard.get(0) || shard.get(0) >= shard.get(1))
+				throw new FatalException(String.format(
+						"shardId must be greater than 0, and totalShards must be greater than shardId! "
+								+ "(Got: shardId:%d, totalShards:%d) "
+								+ "See https://discordapp.com/developers/docs/topics/gateway#sharding",
+						shard.get(0), shard.get(1)));
+		}
 	}
 
-	public abstract void run();
+	abstract void run();
 
-	public static final CLICommand parseCommand(String...args) throws Exception
+	abstract void validateParameters() throws FatalException;
+
+	public final void runCommand() throws FatalException
+	{
+		validateParameters();
+		run();
+	}
+
+	public static final CLICommand parseCommand(String...args) throws FatalException
 	{
 		Map<String,CLICommand> commands = parsePackage.commands;
 		JCommander jc = parsePackage.commandParser;
@@ -112,13 +130,14 @@ public abstract class CLICommand
 		String cmdString = null;
 		try
 		{
-			jc.parse(args);
+			jc.parseWithoutValidation(args);
 			cmdString = jc.getParsedCommand();
 			if (cmdString == null || (cmd = commands.get(cmdString)) == null)
 			{
-				throw new UnknownCommandException("Unrecognized command. Valid commands: " + commands.keySet().toString());
+				throw new UnknownCommandException(
+						"Unrecognized command. Valid commands: " + commands.keySet().toString());
 			}
-		} catch (Exception e)
+		} catch (UnknownCommandException | ParameterException e)
 		{
 			StringBuilder sb = new StringBuilder();
 			// displays nicely in discord code blocks on monitors with resolutions as low as 720p, assuming no strange edge cases
@@ -128,11 +147,14 @@ public abstract class CLICommand
 			else
 				jc.usage(cmdString, sb);
 			System.err.println(sb.toString());
-			throw e;
+			throw new FatalException("Error while parsing command", e);
 		}
 		return cmd;
 	}
 
+	/**
+	 * @author Steven Fontaine
+	 */
 	private static final class UnknownCommandException extends Exception
 	{
 		private static final long serialVersionUID = 8816226238588379400L;
