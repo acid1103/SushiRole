366259ecbc81c2e70a69c3d18cd4c6aec43865c8
Steven Fontaine <sir.fon1103@gmail.com>
2018-10-10 02:19:42 -0500

    add standin test to force push src/test/java

---
+++ src/test/java/org/abitoff/discord/sushirole/SushiRoleTest.java
@@ -0,0 +1,14 @@
+package org.abitoff.discord.sushirole;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SushiRoleTest
+{
+	@Test
+	public void testMethod()
+	{
+		assertTrue("assert true", true);
+	}
+}
